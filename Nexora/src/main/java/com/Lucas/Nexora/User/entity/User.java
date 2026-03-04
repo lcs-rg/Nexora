36f0785cef4ee6cc2472f6320b07752adbe87396
lcs-rg <lucasrochag9@gmail.com>
2026-03-04 16:27:14 -0300

    feat: create User entity with company relationship and role enum

---
+++ Nexora/src/main/java/com/Lucas/Nexora/User/entity/User.java
@@ -0,0 +1,47 @@
+package com.Lucas.Nexora.User.entity;
+
+import com.Lucas.Nexora.Company.entity.Company;
+import com.Lucas.Nexora.Company.enums.CompanyStatus;
+import com.Lucas.Nexora.Company.enums.PlanType;
+import com.Lucas.Nexora.User.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint( columnNames = {"email", "company_id"})
+        }
+)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+}
---
+++ Nexora/src/main/java/com/Lucas/Nexora/User/enums/Role.java
@@ -0,0 +1,7 @@
+package com.Lucas.Nexora.User.enums;
+
+public enum Role {
+    ADMIN,
+    MANAGER,
+    USER
+}
