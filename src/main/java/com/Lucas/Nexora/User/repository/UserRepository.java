a7f4f265e857f9a32e440423a7fbba3e4775a762
lcs-rg <lucasrochag9@gmail.com>
2026-03-07 02:45:11 -0300

    feat: update UserRepository

--- src/main/java/com/Lucas/Nexora/User/repository/UserRepository.java
+++ src/main/java/com/Lucas/Nexora/User/repository/UserRepository.java
@@ -2,10 +2,18 @@ package com.Lucas.Nexora.User.repository;
 
 import com.Lucas.Nexora.User.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    Optional<User> findByEmailAndCompanyId(String email, UUID companyId);
+
+    boolean existsByEmailAndCompanyId(String email, UUID companyId);
+
+    Optional<User> findByEmail(String email);
+
+    List<User> findAllByCompanyId(UUID companyId);
 }
